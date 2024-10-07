17232fbd0e906d8000489467bdcbb5b581637564
Vincenzo Fehring <vinfehring@gmail.com>
2024-10-07 15:24:34 -0500

    Create database connection with GORM

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	db, err := gorm.Open(sqlite.Open("api.db"), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+
+	DB = db
+}
 
 func main() {
 	app := fiber.New()
+
+	ConnectDB()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
