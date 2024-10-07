929aea074ca157d60dbd8054d3e6160aad3f0046
Vincenzo Fehring <vinfehring@gmail.com>
2024-10-07 15:25:20 -0500

    Switched from SQLite to Postgres

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gofiber/fiber/v2"
-	"gorm.io/driver/sqlite"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func ConnectDB() {
-	db, err := gorm.Open(sqlite.Open("api.db"), &gorm.Config{})
+	dsn := os.Getenv("DB_URL")
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect to database")
 	}
